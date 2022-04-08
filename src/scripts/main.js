2ff166a17b583fc66038fff324a5caf0365c2757
AlexAzalor <yablunovsky.a@gmail.com>
2022-04-08 11:45:43 +0300

    create slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
-'use strict';
+import Swiper from 'swiper/swiper-bundle.esm.js';
+import 'swiper/swiper-bundle.css';
+
+export const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
